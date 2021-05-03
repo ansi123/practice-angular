34f121f71843ad8d19016739a89151da62b407d0
ansi123 <mohitsinghalcool1998@gmail.com>
2021-05-03 14:03:15 +0530

    commit for lazy routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NormalComponent} from './normal/normal.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: "admin", loadChildren: () => import('./admin/admin.module')
+      .then(mod => mod.AdminModule)
+  },
+  {
+    path: "user", loadChildren: () => import('./user/user.module')
+      .then(mod => mod.UserModule)
+  },
+  {
+    path:"normal", component: NormalComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+import { Component} from '@angular/core';
+import {ServiceService} from './service.service'
 
 @Component({
+
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'practice-angular';
+  pam:"";
+  constructor(private user:ServiceService){
+    this.user.getData().subscribe(data=>{
+      console.log("data is",data);
+    });
+    
+
+  }
 }
